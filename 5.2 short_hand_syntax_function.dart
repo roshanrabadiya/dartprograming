cba2c16ab5697a1fc5a444421cf08d39fe714b95
roshanrabadiya <39290178+roshanrabadiya@users.noreply.github.com>
2019-09-05 10:48:56 +0530

    Update 5.2 short_hand_syntax_function.dart

--- 5.2 short_hand_syntax_function.dart
+++ 5.2 short_hand_syntax_function.dart
@@ -13,13 +13,11 @@ void main(){
 // this function is also knoen as the 'lambda'
 // when our function contais only one line of code then we use this arrow.
 
-void findPerameters(int length, int breadth) =>
-    print("The perameters is ${2 * (length + breadth)}");
+void findPerameters(int length, int breadth) => print("The perameters is ${2 * (length + breadth)}");
 
 int getArea(int length, int breath) =>  length * breath; 
 //hear the return keyword not needed
 
-void maxNumber(int a, int b) => 
-    a>b ? print("$a is Max.") : print("$b is Max.");
+void maxNumber(int a, int b) => a>b ? print("$a is Max.") : print("$b is Max.");
 
 
